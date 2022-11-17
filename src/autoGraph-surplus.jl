6837e16fb20799d00a069815bdb0c0c9c3ac4171
Raphael Darley <raphael@raphaeldarley.com>
2022-11-17 01:05:36 +0000

    implement surplus graphs

--- src/autoGraph-surplus.jl
+++ src/autoGraph-surplus.jl
@@ -35,15 +35,14 @@ if lowercase(resp) == "y"
     end
 end
 
-sheet_name = ""
 print("Please enter the name of the sheet to graph exactly\n>>>")
-worksheet_name = readline()
-while !contains_string(worksheet_names, worksheet_name)
+worksheet_name1 = readline()
+while !contains_string(worksheet_names, worksheet_name1)
     print("Worksheet not found in file, try again, or quit and select new file.\n>>>")
-    global sheet_name = readline()
+    global worksheet_name1 = readline()
 end
 
-op_worksheet1 = xf[worksheet_name]
+op_worksheet1 = xf[worksheet_name1]
 
 type_col = "A"
 x_row = "3"
@@ -66,13 +65,13 @@ x_end1 = x_range[2]
 
 
 print("Please enter the name of the second sheet to graph exactly\n>>>")
-worksheet_name = readline()
-while !contains_string(worksheet_names, worksheet_name)
+worksheet_name2 = readline()
+while !contains_string(worksheet_names, worksheet_name2)
     print("Worksheet not found in file, try again, or quit and select new file.\n>>>")
-    global sheet_name = readline()
+    global worksheet_name2 = readline()
 end
 
-op_worksheet2 = xf[worksheet_name]
+op_worksheet2 = xf[worksheet_name2]
 
 print("Please enter range of rows for types, currently only ranges are supported, please enter the starting row number followed by a colon and the ending number, the second number should be larger than the starting number eg. 5:30.\n>>>")
 resp = readline()
@@ -94,23 +93,79 @@ out_path = Base.Filesystem.mkpath(out_path)
 # println("start: $type_start")
 # println("end: $type_end")
 
-x_series1 = op_worksheet1["$x_start1$x_row1:$x_end1$x_row1"][:]
-x_series2 = op_worksheet1["$x_start2$x_row2:$x_end2$x_row2"][:]
+x_series1 = op_worksheet1["$x_start1$x_row:$x_end1$x_row"][:]
+x_series2 = op_worksheet2["$x_start2$x_row:$x_end2$x_row"][:]
+
+# println("series 1: $x_series1")
+# println("series 2: $x_series2")
+
 
 if x_series1 != x_series2
     error("X series do not match")
+else
+    x_series = x_series1
 end
 
 data_series1 = Dict()
 
 for i in type_start1:type_end1
-        type_name = op_worksheet["$type_col$i"]
+    type_name = op_worksheet1["$type_col$i"]
     if (typeof(type_name) == Missing)
-        # println("skipped")
         continue
     end
     type_name = strip(type_name)
+
+    series = op_worksheet1["$x_start1$i:$x_end1$i"]
+
+    data_series1[type_name] = series
 end
+
+
+data_series2 = Dict()
+
+for i in type_start2:type_end2
+    type_name = op_worksheet2["$type_col$i"]
+    if (typeof(type_name) == Missing)
+        continue
+    end
+    type_name = strip(type_name)
+
+    series = op_worksheet2["$x_start1$i:$x_end1$i"]
+
+    data_series2[type_name] = series
+end
+
+# println(data_series1)
+# println(data_series2)
+
+for (type_name, series1) in data_series1
+    if !haskey(data_series2, type_name)
+        continue
+    end
+
+    plot_name = "$type_name-$worksheet_name1-$worksheet_name2"
+
+    series1 = series1[:]
+    series2 = data_series2[type_name][:]
+
+    diff_series = [d1 - d2 for (d1, d2) in zip(series1, series2)]
+
+
+    plot_draft = plot(x_series, series1, label=worksheet_name1, legend=:topleft)
+    plot!(plot_draft, x_series, series2, label=worksheet_name2)
+    plot!(plot_draft, x_series, diff_series, label="($worksheet_name1) - ($worksheet_name2)")
+
+    if minimum(diff_series) < 0
+        plot!(plot_draft, (x) -> 0, minimum(x_series), maximum(x_series), label="", line=(:black))
+    end
+
+    title!(plot_draft, plot_name)
+
+    png(plot_draft, "$out_path/$plot_name")
+
+
+end
+
 
 
 
